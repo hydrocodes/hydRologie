6e01183a7a2d77d89ecc2c45d83515da1e178a82
Pedro Rau <60294726+hydrocodes@users.noreply.github.com>
2023-07-05 18:07:48 -0500

    Update Analyse_mensuelle.R

--- Analyse_mensuelle.R
+++ Analyse_mensuelle.R
@@ -1,22 +1,3 @@
-rain<-c(200, 210.2, 490, 100.5, 150.1, 190, 310, 200.2)
-boxplot(rain) 
-boxplot(rain, ylab="P (mm/an)", col="cyan")  
-
-hist(rain, main="Précipitation annuelle", xlab="P (mm/an)", freq=F) 
-# Histogramme, F(False) pour n’est pas utiliser la fréquence
-curve(dnorm(x, mean(rain, na.rm = T), sd(rain, na.rm = T)), add = TRUE, col="red") # Ajouter une courbe de distribution normale en rouge
-split.screen(c(1,2)) #Fonction pour créer une espace des figures, e.g. 2 figures dans 1 ligne et  2 colonnes
-screen(2) #Utiliser cette fonction pour préciser la position avant de tracer la figure
-boxplot(rain, ylab="P (mm/an)", col="cyan")  
-hist(rain, main="Précipitation annuelle", xlab="P (mm/an)", freq=F) 
-# Histogramme, F(False) pour n’est pas utiliser la fréquence
-
-curve(dnorm(x, mean(rain, na.rm = T), sd(rain, na.rm = T)), add = TRUE, col="red") # Ajouter une courbe de distribution normale en rouge
-
-
-close.screen(all=TRUE) # fonction pour retourner aux conditions initiales d’une figure par fenêtre
-cov(rain,elev) #covariance
-
 library("lattice") #Appeler le package lattice
 library("hydroTSM") # Appeler le package hydroTSM
 pmensual<-read.table(file.choose(), header = F, check.names = F) #lecture du fichier avec l’option fenêtre en saillie, chercher la route du fichier et sélectionner, header “F” dans le cas où l’entete ne contient pas de données sinon que de noms
@@ -34,4 +15,3 @@ meses<-pmensual[1:1,2:13] #lecture de l’entete des mois dans la premiere ligne
 colnames(lluvia)<-unlist(meses) #desagregation des noms de mois et attribuer à la matrice
 rownames(lluvia)<-pmensual[2:45,1:1] #désagrégation des dates dans le lignes et attribuer à la matrice 
 matrixplot(lluvia, ColorRamp="Precipitation", main="Précipitation Mensuelle 1964-2007 (mm/mois)")
-
